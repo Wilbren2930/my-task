ca9365f8cc98241fe6168fa038286a334f612ec7
Wilbren Rosario <wilbrenrosario@gmail.com>
2019-08-27 21:22:38 -0400

    Working 87%

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { ModalAddTaskComponent } from './modal-add-task/modal-add-task.component';
-import { ListaComponent } from './lista/lista.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ListaComponent } from './lista/lista.component';
+import { HomeComponent } from './home/home.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { Error404Component } from './error404/error404.component';
 
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule} from '@angular/fire/firestore';
-import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 
 import { environment } from '../environments/environment.prod'; 
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
@@ -21,18 +23,16 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { BackendService } from './backend.service';
 import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './home/home.component';
-import { Error404Component } from './error404/error404.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    ModalAddTaskComponent,
-    ListaComponent,
-    LoginComponent,
     HomeComponent,
-    Error404Component
+    ListaComponent,
+    Error404Component,
+    LoginComponent,
+    ModalAddTaskComponent
   ],
   imports: [
     BrowserModule,
@@ -42,10 +42,11 @@ import { Error404Component } from './error404/error404.component';
     ReactiveFormsModule,
     RouterModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
-    AngularFirestoreModule
+    AngularFirestoreModule,
+    AngularFireAuthModule
 
   ],
-  providers: [AngularFireAuth, BackendService],
+  providers: [ BackendService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
